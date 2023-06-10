3c07cef2bcf3209676c56c685f5f07b0f9066d69
Mayra <90107298+MayraMorandi@users.noreply.github.com>
2023-06-09 23:21:02 -0300

    Add files via upload

---
+++ TurismoEnLaTierraMedia/src/tpTurismo/Usuario.java
@@ -0,0 +1,49 @@
+package tpTurismo;
+
+public class Usuario {
+
+	private String nombreApellido;
+	private String tipoAtraccionPreferida;
+	private int presupuesto;
+	private float horasDisponibles;
+
+	public Usuario(String nombreApellido, String atraccionPreferida, int presupuesto, float horasDisponibles) {
+		this.nombreApellido = nombreApellido;
+		this.tipoAtraccionPreferida = atraccionPreferida;
+		this.presupuesto = presupuesto;
+		this.horasDisponibles = horasDisponibles;
+	}
+
+	public boolean actualizarPresupuestoYTiempo(int precio, float tiempo) {
+
+		if (this.presupuesto >= precio && this.horasDisponibles >= tiempo) {
+			this.presupuesto -= precio;
+			this.horasDisponibles -= tiempo;
+			return true;
+		}
+		return false;
+	}
+
+	public int consultarPresupuesto() {
+		return this.presupuesto;
+	}
+
+	public float consultarTiempo() {
+		return this.horasDisponibles;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nombreApellido=" + nombreApellido + ", tipoAtraccionPreferida=" + tipoAtraccionPreferida
+				+ ", presupuesto=" + presupuesto + ", horasDisponibles=" + horasDisponibles + "]";
+	}
+
+	public String consultarAtraccionPreferida() {
+		return this.tipoAtraccionPreferida;
+	}
+
+	public String consultarNombre() {
+		return this.nombreApellido;
+	}
+
+}
